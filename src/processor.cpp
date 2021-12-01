a2ed86bf02868814e914e2aa1e863721c60ab77f
Marco Savo <savomarco@gmail.com>
2021-12-01 11:53:13 +0100

    Update processor.cpp

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,12 @@
 #include "processor.h"
+#include "linux_parser.h"
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() {
+    long jiffies = LinuxParser::Jiffies();
+    long jiffies_idle = LinuxParser::IdleJiffies();
+    LinuxParser::UpdateJiffies();
+    const float idle_time_delta = LinuxParser::IdleJiffies() - jiffies_idle;
+    const float total_time_delta = LinuxParser::Jiffies() - jiffies;
+    return (100.0 * (1.0 - idle_time_delta / total_time_delta));
+}
